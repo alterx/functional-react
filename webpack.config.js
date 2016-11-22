0f1781f05dabdd70c6a1d173e16c1468d1190d27
Carlos Vega <clmvega@gmail.com>
2016-11-22 14:19:40 -0600

    Removing unused files and requires

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,18 @@
 var path = require('path');
-var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 var loaders = [
   {
-    "test": /\.js?$/,
-    "exclude": /node_modules/,
-    "loader": "babel",
-    "query": {
-      "presets": [
-        "es2015",
-        "react",
-        "stage-0"
+    'test': /\.js?$/,
+    'exclude': /node_modules/,
+    'loader': 'babel',
+    'query': {
+      'presets': [
+        'es2015',
+        'react',
+        'stage-0'
       ],
-      "plugins": []
+      'plugins': []
     }
   }
 ];
