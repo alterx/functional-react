e38baa38979910a2c14eba5b899685a6f27b5c8b
Carlos Vega <clmvega@gmail.com>
2017-02-12 12:18:20 -0600

    No string refs
    Signed-off-by: Carlos Vega <clmvega@gmail.com>

--- src/app.js
+++ src/app.js
@@ -2,17 +2,27 @@ import React from 'react';
 import TodoList from './todo';
 
 class App extends React.Component {
+
   constructor (props) {
     super(props);
 
     this.addToDo = this.addToDo.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      'newTodo': ''
+    };
+  }
+
+  handleChange (event) {
+    this.setState({ newTodo: event.target.value });
   }
 
   addToDo () {
     const { actions } = this.props;
-    if (this.refs.newTodo) {
-      actions.addToDo(this.refs.newTodo.value);
-      this.refs.newTodo.value = '';
+
+    if (this.state.newTodo) {
+      actions.addToDo(this.state.newTodo);
+      this.setState({ 'newTodo': '' });
     }
   }
 
@@ -21,7 +31,12 @@ class App extends React.Component {
 
     return <div className="container">
       <h1>Hello {name}</h1>
-      <input type="text" ref="newTodo" autoFocus />
+      <input
+        type="text"
+        value={this.state.newTodo}
+        onChange={this.handleChange}
+        autoFocus
+      />
       <button onClick={this.addToDo}>Add</button>
       <TodoList items={items} />
     </div>;
